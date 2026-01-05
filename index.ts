1bdc9848bae42e1870c2d7a1e2e3669c2369eef4
Hussein Hegazy <husshegazydev@gmail.com>
2026-01-04 23:44:33 -0500

    Testing out bun full stack dev server

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+import homepage from "./index.html";
+
+const server = serve({
+    routes: {
+        // ** HTML imports **
+        // Bundle & route index.html to "/". This uses HTMLRewriter to scan
+        // the HTML for `<script>` and `<link>` tags, runs Bun's JavaScript
+        // & CSS bundler on them, transpiles any TypeScript, JSX, and TSX,
+        // downlevels CSS with Bun's CSS parser and serves the result.
+        "/": homepage,
+    },
+
+    // Enable development mode for:
+    // - Detailed error messages
+    // - Hot reloading (Bun v1.2.3+ required)
+    development: true,
+});
+
+console.log(`Listening on ${server.url}`);
